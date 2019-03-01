2332d7ce2fb336e94636fd1dc0f358a1f42c58ce
Myaka <egendron6@icloud.com>
2019-03-01 13:57:27 +0100

    add code json pour ifttt

---
+++ script/script.js
@@ -0,0 +1,18 @@
+//Json à copier dans l'application web ifttt 
+
+{
+  "content": "Mise à jour du répertoire : {{RepositoryName}}",
+  "embeds": [
+    {
+    "footer": {
+      "text": "app crée par Myaka",
+      "icon_url": "https://image.noelshack.com/fichiers/2019/08/7/1550965067-rem-gyate-re-zero-by-justfream-dcsi6i8-250t.png"},
+    "thumbnail": {
+      "url": "https://image.noelshack.com/fichiers/2019/08/7/1550965134-4.png"},
+    "title": "{{IssueTitle}}",
+    "description": "{{IssueBody}} \n par: {{AuthorUsername}}",
+
+    "color": 16777215
+  }
+ ]
+}
